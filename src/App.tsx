8ace48f2e5b7084d8982267fefb84d6455f090c6
Lucas Araújo <lucascvalle@outlook.com>
2025-08-11 12:41:08 +0100

    feat: Implement Vercel Analytics

--- src/App.tsx
+++ src/App.tsx
@@ -13,6 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
+      <Analytics />
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
